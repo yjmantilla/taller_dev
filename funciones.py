ffbd7d5cef2890ef353eae757488d6c686e7e332
yeimmygit11 <yeimmy1197@gmail.com>
2021-11-12 11:55:21 -0500

    Ultimos cambios

--- funciones.py
+++ funciones.py
@@ -1,10 +1,7 @@
 def yorguin():
    print('yorguin')
 
-<<<<<<< HEAD
 def yeimmy():
    print("Yeimmy Morales")
-=======
 def taller():
    print('taller')
->>>>>>> 291ce9f1e8a9e1eee145085ebb32af77708042e4
