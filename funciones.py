5da6d93c7f3059f82edcec0f5548e63adc18926d
veronicahenaoisaza <veronica.henaoi@udea.edu.co>
2021-11-12 11:47:50 -0500

    actualización vero

--- funciones.py
+++ funciones.py
@@ -1,10 +1,7 @@
 def yorguin():
    print('yorguin')
 
-<<<<<<< HEAD
 def veronica():
    print('verónica')
-=======
 def taller():
    print('taller')
->>>>>>> 291ce9f1e8a9e1eee145085ebb32af77708042e4
